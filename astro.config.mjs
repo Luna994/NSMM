4f38a121765d4b38a47465bd777e10c5396203cf
Luna994 <yuliamik@gmail.com>
2025-02-25 16:38:39 +0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   base: '/NSMM/', // Замени на имя своего репозитория
-  site: 'https://USERNAME.github.io/NSMM/',
+  site: 'https://Luna994.github.io/NSMM/',
 });
